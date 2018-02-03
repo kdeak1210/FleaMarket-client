8edf4677cfdf51aaa9ed63f20e5dcf73250bd047
kyle <kdeak1210@gmail.com>
2018-02-03 16:36:48 -0500

    Initial commit
    Create React App housecleaning, eslint config

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount() {
+    console.log('hello');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
