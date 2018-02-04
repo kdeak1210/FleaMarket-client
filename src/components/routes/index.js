0a43ceae3769319122590f42f2dba59bda89cc85
kyle <kdeak1210@gmail.com>
2018-02-04 18:46:37 -0500

    Incorporated React-Router, placeholder login/register routes

---
+++ src/components/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+
+export default () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+);
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Home from './components/routes/Home';
+import Routes from './components/routes';
 import store from './stores';
 
 // Configure Redux store w/ null initialState (no SSR now)
 const App = (
   <Provider store={store.configure(null)} >
-    <Home />
+    <Routes />
   </Provider>
 );
 
